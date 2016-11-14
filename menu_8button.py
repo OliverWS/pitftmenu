f69fb03d24e6ac296faa508a5cca9e3240c3afac
OliverWS <OliverWS@gmail.com>
2016-11-14 15:28:21 -0500

    Update menu_8button.py
    Object-orient-ify and make extensible

--- menu_8button.py
+++ menu_8button.py
@@ -4,6 +4,7 @@ import time
 import subprocess
 import os
 from subprocess import *
+import json
 os.environ["SDL_FBDEV"] = "/dev/fb1"
 os.environ["SDL_MOUSEDEV"] = "/dev/input/touchscreen"
 os.environ["SDL_MOUSEDRV"] = "TSLIB"
@@ -12,18 +13,32 @@ os.environ["SDL_MOUSEDRV"] = "TSLIB"
 pygame.init()
 pygame.mouse.set_visible(0)
 
+#colors     R    G    B
+white   = (255, 255, 255)
+red     = (255,   0,   0)
+green   = (  0, 255,   0)
+blue    = (  0,   0, 255)
+black   = (  0,   0,   0)
+cyan    = ( 50, 255, 255)
+magenta = (255,   0, 255)
+yellow  = (255, 255,   0)
+orange  = (255, 127,   0)
+
+
 class Button:
-    def __init__(self,x,y,w,h,handler=None):
+    def __init__(self,x,y,w,h,text="Button", colour=green,handler=None):
         self.x = x
         self.y = y
         self.h = h
         self.w = w
         self.handler = handler
+        self.text = text
+        self.colour=colour
         self.render()
     
     def render(self):
         font=pygame.font.Font(None,42)
-        label=font.render(str(text), 1, (colour))
+        label=font.render(str(self.text), 1, (self.colour))
         screen.blit(label,(xpo,ypo))
         pygame.draw.rect(screen, blue, (xpo-border,ypo-border,width,height),3)
 
@@ -39,71 +54,29 @@ class Button:
             return False
 
 
-# define function for printing text in a specific place with a specific width and height with a specific colour and border
-def make_button(text, xpo, ypo, height, width, colour,border=10,handler=None):
-    font=pygame.font.Font(None,42)
-    label=font.render(str(text), 1, (colour))
-    screen.blit(label,(xpo,ypo))
-    pygame.draw.rect(screen, blue, (xpo-border,ypo-border,width,height),3)
-    return Rect(xpo,ypo,height,width)
-
-# define function for printing text in a specific place with a specific colour
-def make_label(text, xpo, ypo, fontsize, colour):
-    font=pygame.font.Font(None,fontsize)
-    label=font.render(str(text), 1, (colour))
-    screen.blit(label,(xpo,ypo))
-
-# Define each button press action
-def button(number):
-    print "You pressed button ",number
-
-    if number == 1:
-        time.sleep(5) #do something interesting here
-        sys.exit()
-
-    if number == 2:
-        time.sleep(5) #do something interesting here
-        sys.exit()
-
-    if number == 3:
-        time.sleep(5) #do something interesting here
-        sys.exit()
-
-    if number == 4:
-        time.sleep(5) #do something interesting here
-        sys.exit()
-
-    if number == 5:
-        time.sleep(5) #do something interesting here
-        sys.exit()
-
-    if number == 6:
-        time.sleep(5) #do something interesting here
-        sys.exit()
-
-    if number == 7:
-        time.sleep(5) #do something interesting here
-        sys.exit()
-
-    if number == 8:
-        time.sleep(5) #do something interesting here
-        sys.exit()
-
-#colors     R    G    B
-white   = (255, 255, 255)
-red     = (255,   0,   0)
-green   = (  0, 255,   0)
-blue    = (  0,   0, 255)
-black   = (  0,   0,   0)
-cyan    = ( 50, 255, 255)
-magenta = (255,   0, 255)
-yellow  = (255, 255,   0)
-orange  = (255, 127,   0)
 
 # Set up the base menu you can customize your menu with the colors above
+btn_config = [
+    {"label":"Button 1","handler":"sudo raspi-config", colour=[255,0,0]},
+    {"label":"Button 2","handler":"sudo raspi-config", colour=[255,0,255]},
+    {"label":"Button 3","handler":"sudo raspi-config", colour=[255,0,0]},
+    {"label":"Button 4","handler":"sudo raspi-config", colour=[255,0,255]},
+    {"label":"Button 5","handler":"sudo raspi-config", colour=[255,0,0]},
+    {"label":"Button 6","handler":"sudo raspi-config", colour=[255,0,255]},
+    {"label":"Button 7","handler":"sudo raspi-config", colour=[255,0,0]},
+    {"label":"Button 8","handler":"sudo raspi-config", colour=[255,0,255]},
+]
+config = {
+    "width":320,
+    "height":240,
+    "buttons":btn_config
+}
 
+try:
+    with open("menu_config.json","r") as f:
+        config = json.load(f)
 #set size of the screen
-size = width, height = 320, 240
+size = width, height = config["width"], config["height"]
 border = 10
 screen = pygame.display.set_mode(size)
 
@@ -119,30 +92,18 @@ pad = width/16
 nRows = 4
 nCols = 2
 
+
 buttons = []
 def dummy_function(n):
     print "Button %d pushed"%(n)
+n = 0
+
+
 for row in range(0,nRows):
-    for col in range(,nCols):
-        b = Button(pad*(col+1)+button_width*col,pad*(row+1)+button_height*row,button_width, button_height, dummy_function)
+    for col in range(,nCols):            
+        b = Button(pad*(col+1)+button_width*col,pad*(row+1)+button_height*row,button_width, button_height,config[n]["label"],config[n]["colour"],lambda: os.system(config[n]) )
         buttons.append(b)
-
-# # Buttons and labels
-# # First Row
-# make_button("Menu Item 1", 30, 30, 55, 210, blue)
-# make_button("Menu Item 2", 260, 30, 55, 210, blue)
-# # Second Row
-# make_button("Menu Item 3", 30, 105, 55, 210, blue)
-# make_button("Menu item 4", 260, 105, 55, 210, blue)
-# # Third Row
-# make_button("Menu item 5", 30, 180, 55, 210, blue)
-# make_button("Menu item 6", 260, 180, 55, 210, blue)
-# # Fourth Row
-# make_button("Menu item 7", 30, 255, 55, 210, blue)
-# make_button("Menu item 8", 260, 255, 55, 210, blue)
-
-# While loop to manage touch screen inputs
-
+        n=n+1
 
 # define function that checks for touch location
 def on_touch():
@@ -150,9 +111,6 @@ def on_touch():
     touchx,touchy = (pygame.mouse.get_pos() [0], pygame.mouse.get_pos() [1])
     for b in buttons:
         b.hit_test(touchx,touchy)
-
-
-
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -161,9 +119,6 @@ while 1:
             print pos #for checking
             pygame.draw.circle(screen, white, pos, 2, 0) #for debugging purposes - adds a small dot where the screen is pressed
             on_touch()
-
-#ensure there is always a safe way to end the program if the touch screen fails
-
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 sys.exit()
